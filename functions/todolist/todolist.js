aa572d21eac58bcca9c56698e50ad980a7ec8a1d
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-11-30 11:24:37 +0500

    TODO!

---
+++ functions/todolist/todolist.js
@@ -0,0 +1,90 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const faunadb = require('faunadb'),
+      q = faunadb.query;
+
+ require("dotenv").config()
+const typeDefs = gql`
+  type Query {
+    todos: [Todo]
+  }
+  type Mutation {
+    addTodo(text: String!): String
+    removeTodo(id: String!): String
+  }
+  type Todo {
+    id: String
+    text: String
+    done: Boolean!
+  }
+`
+
+const resolvers = {
+  Query: {
+    todos: async (root, args, context) => {
+      try {
+        const adminClient = new faunadb.Client({ secret: process.env.FAUNA_SECRETS });
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index('task'))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+
+        console.log(result.data);
+        
+        return result.data.map( (d) => {
+          return {
+            id: d.ts,
+            text: d.data.text,
+            done: d.data.done
+          }
+        })
+      }
+      catch(error) {
+        console.log(error);
+      }
+    },
+  },
+  Mutation: {
+      addTodo: async (_, { text }) => {
+      try {
+        const adminClient = new faunadb.Client({ secret: process.env.FAUNA_SECRETS  });
+        const result = await adminClient.query(
+          q.Create(
+            q.Collection('todos'),
+            { data: {
+              text: text,
+              done: false
+            }},
+          )
+        )
+        
+        console.log(result)
+        return result.ref.data;
+      } catch (error) {
+        return error.toString()
+      }
+    },
+    removeTodo: async (_, { id }) => {
+    try {
+      const adminClient = new faunadb.Client({ secret: process.env.FAUNA_SECRETS  });
+        const result = await adminClient.query(
+        q.Delete(q.Ref(q.Collection("todos"), id))
+      )
+
+      return result.id.toString()
+    } catch (error) {
+      return error.toString()
+    }
+  },
+},
+}
+ 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = server.createHandler()
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,90 @@
-import React from "react"
+import React from "react";
+import { useQuery, useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import "./style.css";
+import { Delete } from "@material-ui/icons";
+
+const GET_TODO = gql`
+  {
+    todos {
+      id,
+      text,
+      done
+    }
+  }
+`;
+
+const ADD_TODO = gql`
+  mutation addTodo($text: String!){
+    addTodo(text: $text)
+  }
+`;
+const DELETE_TODO = gql`
+  mutation RemoveTodo($id: String!) {
+    removeTodo(id: $id)
+  }
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  let textInput;
+
+  const [addTodo] = useMutation(ADD_TODO);
+  
+  const [removeTodo] = useMutation(DELETE_TODO)
+
+  const addNewTodo = () => {
+    addTodo({
+      variables: {
+        text: textInput.value
+      },
+      refetchQueries: [{ query: GET_TODO }]
+    })
+    textInput.value = "";
+  }
+  const handleDelete = (id)=> {
+    removeTodo({
+      variables: { id: id },
+      refetchQueries: [{ query: GET_TODO }],
+    })
+      }
+  const { loading, error, data } = useQuery(GET_TODO);
+
+  if ( loading ) {
+    return <h2>Loading...</h2>
+  }
+  
+  if ( error ) {
+    return <h2>Loading Error </h2>
+  }
+
+  return (
+    <div className="container">
+      <h1>My Todo List</h1>
+      <form className="add_todo">
+        <label>
+          <input type="text" required ref={node => {textInput = node}} placeholder="Add Todo..." />
+        </label>
+        <button onClick={addNewTodo}>Add</button>
+      </form>
+      <ul>
+        {data?.todos.map(todo => 
+          <li key={todo.id}>
+            <div className="listTodo">
+                <div>
+
+                {todo.text}
+                </div>
+                <div className="icons">
+                <Delete 
+                 edge="end"
+                 aria-label="delete" 
+                onClick={()=> handleDelete(todo.id)} />
+                </div>
+               
+
+                </div>
+          </li>)}
+      </ul>
+    </div>
+  )
 }
